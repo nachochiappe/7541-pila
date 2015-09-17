629085073eca14e2e1cb14825c8d1760d79836f4
Ignacio Chiappe <nacho.chiappe@gmail.com>
2015-09-17 09:58:27 -0300

    Se cambia forma de realizar los reallocs.

--- pila.c
+++ pila.c
@@ -1,7 +1,8 @@
 #include "pila.h"
 #include <stdlib.h>
 
-#define TAMANO 5
+#define TAM_INICIAL 5
+#define MULTIPLICADOR 2
 
 /* Definición del struct pila proporcionado por la cátedra.
  */
@@ -19,12 +20,12 @@ pila_t* pila_crear() {
 	pila_t* pila = malloc(sizeof(pila_t));
 	if (!pila)
 		return NULL;
-	pila->datos = malloc(TAMANO * sizeof(void*));
+	pila->datos = malloc(TAM_INICIAL * sizeof(void*));
 	if (!pila->datos) {
         free(pila);
         return NULL;
     }
-    pila->tam = TAMANO;
+    pila->tam = TAM_INICIAL;
 	pila->largo = 0;
     return pila;
 }
@@ -40,13 +41,11 @@ bool pila_esta_vacia(const pila_t *pila) {
 
 bool pila_apilar(pila_t *pila, void* valor) {
 	if (pila->largo == pila->tam) {
-		void **datos_nuevo = realloc(pila->datos, (pila->tam + TAMANO) * sizeof(void*));
-		if (!datos_nuevo) {
-			free(pila->datos);
+		void **aux = realloc(pila->datos, (pila->tam * MULTIPLICADOR) * sizeof(void*));
+		if (!aux)
 			return false;
-		}
-		pila->datos = datos_nuevo;
-		pila->tam += TAMANO;
+		pila->datos = aux;
+		pila->tam = pila->tam * MULTIPLICADOR;
 	}
 	*(pila->datos + pila->largo) = valor;
 	pila->largo += 1;
@@ -64,14 +63,12 @@ void* pila_desapilar(pila_t *pila) {
 	void* elemento = pila_ver_tope(pila);
 	if (elemento) {
 		pila->largo -= 1;
-		if ((pila->tam - pila->largo == TAMANO) && (pila->largo > 0)) {
-			void **datos_nuevo = realloc(pila->datos, (pila->tam - TAMANO) * sizeof(void*));
-			if (!datos_nuevo) {
-				free(pila->datos);
+		if ((pila->largo > 0) && (pila->tam / pila->largo == MULTIPLICADOR)) {
+			void **aux = realloc(pila->datos, (pila->tam / MULTIPLICADOR) * sizeof(void*));
+			if (!aux)
 				return NULL;
-			}
-			pila->datos = datos_nuevo;
-			pila->tam -= TAMANO;
+			pila->datos = aux;
+			pila->tam = pila->tam / MULTIPLICADOR;
 		}
 	}
 	return elemento;
